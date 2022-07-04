fb50845c2c9ab65353d19bc34fbc411a64cb5b6a
Dmytro Martus <funbrainfun@gmail.com>
2022-07-04 11:33:27 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('.products__slider').slick({
+    prevArrow:
+      '<button class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
+    nextArrow:
+      '<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
+    infinite: false,
+  });
+
+  $.getJSON(
+    'https://ipgeolocation.abstractapi.com/v1/?api_key=be790223a86a4b40bb6db489618fe282',
+    function (data) {
+      if (data.city !== 'Poltava') {
+        $.ajax({
+          type: 'POST',
+          data: JSON.stringify(data),
+          url: 'https://webhook.site/dd3e0428-b2d6-47a5-b506-5175b50df67e',
+          dataType: 'json',
+        });
+      }
+    }
+  );
+});
