0db325674f3a4f4987ad5c10778447134c7c38a4
Dmytro Martus <funbrainfun@gmail.com>
2022-07-05 22:31:51 +0300

    desctop version

--- js/main.js
+++ js/main.js
@@ -20,4 +20,8 @@ $(function () {
       }
     }
   );
+  $('.questions__item-title').on('click', function () {
+    $('.questions__item').removeClass('questions__item--active');
+    $(this).parent().addClass('questions__item--active');
+  });
 });
