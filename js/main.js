7c907d9257f87a3f1d93e6d336e5efe0ad11c843
Dmytro Martus <funbrainfun@gmail.com>
2022-07-13 10:49:00 +0300

    partly responseve

--- js/main.js
+++ js/main.js
@@ -52,5 +52,10 @@ $(function () {
 
   $('.menu__btn').on('click', function () {
     $(this).toggleClass('menu__btn--active');
+    $(this).next().toggleClass('menu__list--active');
+  });
+  $('.menu__list-link').on('click', function () {
+    $('.menu__btn').removeClass('menu__btn--active');
+    $('.menu__list').removeClass('menu__list--active');
   });
 });
